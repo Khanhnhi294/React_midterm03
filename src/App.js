7eeadd640cfa6e9421efc1cd023d1167b8719107
unknown <admin@pnvl199.pn.com.vn>
2024-05-22 11:15:10 +0700

    Nhi/done/showUser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react"; // Added useState import
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/User"; // Assuming Users is in the components folder
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+  const fetchData = async () => {
+  try {
+  const response = await axios.get("https://api.github.com/users");
+  setUsers(response.data);
+  } catch (error) {
+
+  console.error("Error fetching data:", error);
+  }
+  };
+  fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+  <Navbar />
+  <div className="container">
+  <h1>GitHub Users Data</h1>
+  </div>
+  <Users users={users} />
+  </div>
   );
-}
-
-export default App;
+ };
+ export default App;
+ 
