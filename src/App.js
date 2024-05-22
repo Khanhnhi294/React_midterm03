1fdcdadc6af05e38a0529f56580fa0e354462898
unknown <admin@pnvl199.pn.com.vn>
2024-05-22 11:29:54 +0700

    Nhi/feat/doneShowSearch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react"; // Added useState import
+import { useEffect, useState } from "react"; 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/User"; // Assuming Users is in the components folder
+// import Users from "./components/users/User"; 
+import Search from "./components/users/Search";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-  const fetchData = async () => {
-  try {
-  const response = await axios.get("https://api.github.com/users");
-  setUsers(response.data);
-  } catch (error) {
-
-  console.error("Error fetching data:", error);
-  }
-  };
-  fetchData();
-  }, []);
   return (
   <div className="App">
   <Navbar />
   <div className="container">
   <h1>GitHub Users Data</h1>
+  <Search />
   </div>
-  <Users users={users} />
   </div>
   );
  };
  export default App;
- 
